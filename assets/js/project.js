02600167bb761086b7714c33a56cf163b902d5c1
Md. Hossain Akash <mdakash41gb@gmail.com>
2021-09-15 08:27:54 +0600

    added khujun href

--- assets/js/project.js
+++ assets/js/project.js
@@ -52,7 +52,8 @@ const showCards = () => {
   let output = "";
   projects.forEach(
     ({ title, cardImage, tags, Previewlink, Githublink }) => {
-      (output += `       
+      (output += `   
+      
         <div class="column skill-card card" style="margin: 15px"data-aos="zoom-in-up" data-aos-easing="linear" data-aos-delay="300" data-aos-duration="600" >
           <div class="wrapper" style="background: url(${cardImage}) center / cover no-repeat;">
             <div class="header">
@@ -60,7 +61,7 @@ const showCards = () => {
             <div class="data">
               <div class="content">
               <div class="title-div">
-                <h1 class="title"><a href="#">${title}</a></h1>
+                <h1 class="title"><a href="${Previewlink}">${title}</a></h1>
                 </div>
             <ul class="menu-content"><br>
                   <li><a href="${Previewlink}" class="social-icon"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" viewBox="0 0 30 28" fill="none" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-monitor"><rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect><line x1="8" y1="21" x2="16" y2="21"></line><line x1="12" y1="17" x2="12" y2="21"></line></svg></a></li>
@@ -69,7 +70,8 @@ const showCards = () => {
               </div>
             </div>
           </div>
-        </div>`
+        </div>
+        `
       )
     }
   );
